548be63c6268648c164d95aca9f3ae103d762a00
Floriana-1 <florencesylvanus@yahoo.com>
2022-07-04 11:59:19 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
 */
 int main(void)
 {
-fwrite(2, "and that piece of art is useful" - 'Dora') Korpar, 2015-10-19 \n","61);
+fwrite(2, "and that piece of art is useful" -Dora Korpar, 2015-10-19 \n","61);i
 return (1);
 }
