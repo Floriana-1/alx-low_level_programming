c0135e0d08919efe06a9cc66263d13790e9da184
Floriana-1 <florencesylvanus@yahoo.com>
2022-07-04 07:59:25 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
-* main - Entry poit
-* Return: Always 0 (success)
+*main - Entry poit
+*Return: Always 0 (success)
 */
 int main(void)
 {
-	puts("Programming is like building a multiligual puzzle\n");
-	return(0);
-} 
+puts("Programming is like building a multiligual puzzle\n");
+return (0);
+}
