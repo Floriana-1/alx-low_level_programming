cbcb7f613c2cbd786bcbbf79d6b8e3d602233a8b
Floriana-1 <florencesylvanus@yahoo.com>
2022-07-06 05:44:22 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,14 @@
-/**
-*2-print_alphabet.c
-*/
 #include <stdio.h>
 /**
-* main-Main function goes here
+* main - Main function goes here
 *
 * Return: Always 0 (success)
 */
 int main(void)
 {
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
-	putchar('\n');
-
-	return (0);
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
 }
