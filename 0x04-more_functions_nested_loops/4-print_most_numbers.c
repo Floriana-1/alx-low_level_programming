db7e5b53423be45ab808d1a4b07527d6cc315033
Floriana-1 <florencesylvanus@yahoo.com>
2022-07-07 12:51:56 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_most_numbers - checks for checks for a digit (0 thrugh 9).
+* Return: Always return 0.
+*/
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		if (a != 50)
+		{
+			if (a != 52)
+			{
+			_putchar(a);
+			}
+		}
+	}
+	_putchar('\n\);
+}
