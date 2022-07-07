96ed3cf5d0faa1e4f5d8328c82ed7a16670af30a
Floriana-1 <florencesylvanus@yahoo.com>
2022-07-07 08:10:31 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isdigit - checks for checks for a digti (0 through 9).
+* @c: c - variable
+* Return: Always return 0.
+*/
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
