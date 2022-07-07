7194c7b25f6058605f152541aa28f9c5d1706297
Floriana-1 <florencesylvanus@yahoo.com>
2022-07-07 13:27:18 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,18 +6,22 @@
 */
 void more_numbers(void)
 {
-	int a, b;
+	int a = 0;
+	int b = 0;
 
-	for (a = 0; a < 15; a++)
+	while (b <= 9)
 	{
-		for (b = 0; b < 15; b++)
+		while (a <= 14)
 		{
-			if (b / 10 != 0)
+			if (a > 9)
 			{
-				_putchar(b / 10 + '0');
+				_putchar(a / 10 + '0');
 			}
-			_putchat(b % 10 + '0');
+			_putchat(a % 10 + '0');
+			a++;
 		}
 		_putchar('\n');
+		b++
+		a = 0;
 	}
 }
