b5b3183e1a94b5b6307c6e9ce44f181cc761ee83
Floriana-1 <florencesylvanus@yahoo.com>
2022-07-08 07:23:23 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,15 +10,15 @@ void print_square(int size)
 {
 	int x, y;
 
-	if (size <= 0)
-		_putchar('\n')
+	if (size > 0)
+		_putchar('\n');
 	else
 	{
 		for (x = 0; x < siz; x++)
 		{
 			for (y = 0; y < size; y++)
 			{
-				_putchar('35);
+				_putchar(35);
 			}
 			_putchar('\n');
 		}
