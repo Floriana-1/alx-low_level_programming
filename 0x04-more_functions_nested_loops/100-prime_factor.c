c56f6b311a8102248309e7ecdf37620fbd806fbb
Floriana-1 <florencesylvanus@yahoo.com>
2022-07-08 13:26:45 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h|>
+
+/**
+* main - Entry point
+* Return: 0
+*/
+
+int main(void)
+{
+	long dinum = 612852475143, i;
+
+	while (i < (dinum / 2))
+	{
+		if ((dinum % 2) == 0)
+		{
+			dinum /= 2;
+			continue;
+		}
+
+		for (i = 3; i < (dinum / 2); i += 2)
+		{
+		if ((dinum % i) == 0)
+			dinum /= i;
+		}
+	}
+	printf("%ld\n", dinum);
+
+	return (0);
+}
