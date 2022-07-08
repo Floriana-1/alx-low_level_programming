009bbd2b256bf90c1cc56237636b8e6f91e6fe26
Floriana-1 <florencesylvanus@yahoo.com>
2022-07-08 06:54:55 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,21 +10,16 @@ void print_diagonal(int n)
 {
 	int x, y;
 
-	if (n > 0)
-	{
-		for (x = 1; x <= n; x++)
-		{
-			if (x == y)
-			{
-				_putchar(92);
-				break;
-			}
-			_putchar(' ');
-		}
+	if (n <= 0)
 		_putchar('\n');
-	}
 	else
 	{
-		_putchar('\n');
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y <= x; y++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
 	}
 }
