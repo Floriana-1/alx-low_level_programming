dc2c2f6a76d0ff8c116138560a2992f55ed289b4
Floriana-1 <florencesylvanus@yahoo.com>
2022-07-11 04:42:13 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,14 @@
-#include <stdio.h>
+#include <unistd.h>
+
 /**
-*main-Entrypoint
-*Return:Always1(success)
+* main - Entry point
+* Description - Printing stuffs
+* Return: Always (Success)
 */
+
 int main(void)
 {
- 	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }
